a76366f51251509e302c627c4c7c1ce22b3de971
luna++ <paulkingdev7@gmail.com>
2025-08-10 15:50:12 +0300

    Arcjet error fix

--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -4,7 +4,7 @@ const arcjetMiddleware = async (req, res, next) => {
     try {
         const decision = await aj.protect(req, {requested: 1})
 
-        if (decision.isDenied) {
+        if (decision.isDenied()) {
             if (decision.reason.isRateLimit()) return res.status(429).json({message: 'Too many requests, Rate limit exceeded'});
             if (decision.reason.isBot()) return res.status(403).json({message: 'Bot detected'});
 
