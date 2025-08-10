4b44137df6db426e21ab453b6dce502ca71fc83a
luna++ <paulkingdev7@gmail.com>
2025-08-10 17:04:03 +0300

    single user subscription

--- controller/subscription.controller.js
+++ controller/subscription.controller.js
@@ -1,6 +1,6 @@
 import Subscription from "../models/subscription.model.js";
 
-const createSubscription = async (req, res, next) => {
+export const createSubscription = async (req, res, next) => {
     try {
         const subscription = await Subscription.create({
             ...req.body,
@@ -12,4 +12,18 @@ const createSubscription = async (req, res, next) => {
     }
 }
 
-export default createSubscription;
+export const getuserSubscriptions = async (req, res, next) => {
+    try {
+        // check if the user is the same as the one in the token
+        if (req.user.id !== req.params.id) {
+            const error = new Error('You are not the owner of this account');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        const subscriptions = await Subscription.find({user: req.params.id});
+        res.status(200).json({success: true, data: subscriptions})
+    } catch (error) {
+        next(error);
+    }
+}
